ffcd9ce1d255e0f4412057604ac7b95d31dc12b0
Kate Korsaro <kate.korsaro@gmail.com>
2025-09-27 08:04:42 +0200

    feat: add utility function to set and unset an attribute from an entity

--- 01.workspace/heave/src/str/entity.rs
+++ 01.workspace/heave/src/str/entity.rs
@@ -25,4 +25,19 @@ impl O {
             Some(attribute) => Some(&attribute.value),
         }
     }
+    pub fn set(&mut self, id: &str, value: Value) -> Option<Value> {
+        let attribute = Attribute::new(id, value);
+        let previous_attribute = self.attributes.insert(attribute.id.clone(), attribute);
+        match previous_attribute {
+            None => None,
+            Some(attribute) => Some(attribute.value),
+        }
+    }
+    pub fn unset(&mut self, id: &str) -> Option<Value> {
+        let attribute = self.attributes.remove(id);
+        match attribute {
+            None => None,
+            Some(attribute) => Some(attribute.value),
+        }
+    }
 }
