0e54a7c62d2b4dc932c7af1bb35789fe26c224f5
Kate Korsaro <kate.korsaro@gmail.com>
2025-10-08 16:51:06 +0200

    feat: add with_opt_attribute convenince function to handle options

--- 01.workspace/heave/src/str/entity.rs
+++ 01.workspace/heave/src/str/entity.rs
@@ -35,6 +35,13 @@ impl O {
         self.attributes.insert(attribute.id.clone(), attribute);
         self
     }
+    pub fn with_opt_attribute(mut self, id: &str, value: Option<impl Into<Value>>) -> Self {
+        if let Some(value) = value {
+            let attribute = Attribute::new(id, value);
+            self.attributes.insert(attribute.id.clone(), attribute);
+        }
+        self
+    }
     pub fn value_of(&self, id: &str) -> Option<&Value> {
         let attribute = self.attributes.get(id);
         match attribute {
