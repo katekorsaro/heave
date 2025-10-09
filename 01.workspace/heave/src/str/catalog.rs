9d770d9a33f57b3002c045b3899b8aaba01d52a6
Kate Korsaro <kate.korsaro@gmail.com>
2025-10-09 15:34:01 +0200

    doc: add doc comments to ctalog struct description

--- 01.workspace/heave/src/str/catalog.rs
+++ 01.workspace/heave/src/str/catalog.rs
@@ -1,11 +1,14 @@
 use crate::*;
 
+/// Represents a catalog of entities that can be persisted to a SQLite database.
+///
+/// The `Catalog` holds entities in memory and provides methods to interact with
+/// them, as well as to persist changes to and load data from a database file.
 #[derive(Debug, Default, PartialEq, Clone)]
 pub struct O {
     path: String,
     pub(crate) items: std::collections::HashMap<String, Entity>,
 }
-
 impl O {
     /// Creates a new `Catalog` instance.
     ///
