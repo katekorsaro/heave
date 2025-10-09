6e6cfe036fa3441f5dec16111b7f5413462502df
Kate Korsaro <kate.korsaro@gmail.com>
2025-10-09 15:20:09 +0200

    doc: add entity_state doc comments

--- 01.workspace/heave/src/str/entity_state.rs
+++ 01.workspace/heave/src/str/entity_state.rs
@@ -1,8 +1,13 @@
+/// Represents the state of an entity within the catalog.
 #[derive(Debug, Default, PartialEq, PartialOrd, Eq, Ord, Clone, Copy, Hash)]
 pub enum E {
+    /// The entity is newly created and has not been persisted.
     #[default]
     New,
+    /// The state of the entity is unknown.
     Unknown,
+    /// The entity has been loaded from the database.
     Loaded,
+    /// The entity is marked for deletion.
     ToDelete,
 }
