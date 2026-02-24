15f8c9c09ace7d9781accf7a293a2db54f5ad1d5
katekorsaro <kate.korsaro@gmail.com>
2026-02-24 07:36:15 +0100

    feat: add a new index for entity_id in table attribute

--- 01.workspace/heave/src/fun/sqlite_init_db.rs
+++ 01.workspace/heave/src/fun/sqlite_init_db.rs
@@ -23,6 +23,7 @@ pub fn run(path: &path::Path) -> result::Result<(), FailedTo> {
         CREATE INDEX IF NOT EXISTS entity_class ON entity (class);
         CREATE INDEX IF NOT EXISTS entity_subclass ON entity (subclass);
         CREATE INDEX IF NOT EXISTS attribute_id ON attribute (id);
+        CREATE INDEX IF NOT EXISTS attribute_entity_id ON attribute (entity_id);
         "#;
     let connection = Connection::open(path).map_err(|_| sqlite::FailedTo::OpenConnection)?;
     connection
