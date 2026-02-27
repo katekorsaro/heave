d57022e40da4045f93d4a0d679c99da5c171c49e
katekorsaro <kate.korsaro@gmail.com>
2026-02-27 07:47:32 +0100

    chore: make entity column names explicity during query in load by class

--- 01.workspace/heave/src/fun/sqlite_load_by_class.rs
+++ 01.workspace/heave/src/fun/sqlite_load_by_class.rs
@@ -2,7 +2,7 @@ use crate::*;
 use rusqlite::*;
 
 const SELECT_ENTITY_BY_CLASS: &str = r#"
-    SELECT * FROM entity
+    SELECT id, class, subclass, ref_date FROM entity
     WHERE class = ?1;
 "#;
 
